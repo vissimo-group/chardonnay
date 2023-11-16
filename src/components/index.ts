8260f54d6d75d734cdede6ec39d2cad7bf545e03
vermeuln <vermeuln@ucl.br>
2023-11-16 12:13:40 -0300

    TEC-4070 file main export component create

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import Button from './Button'
+import CardAddress from './CardAddress'
+import Input from './Input'
+import Message from './Message'
+import Modal from './Modal'
+import Tag from './Tag'
+import Tooltip from './Tooltip'
+import TopBar from './TopBar'
+
+export { Button, CardAddress, Input, Message, Modal, Tag, Tooltip, TopBar }
