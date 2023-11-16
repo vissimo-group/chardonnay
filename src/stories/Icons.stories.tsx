8423166d8c91f3a3e71c12f2672c3b588097a2b4
vermeuln <vermeuln@ucl.br>
2023-11-16 12:23:53 -0300

    TEC-4070 stories Icons

---
+++ src/stories/Icons.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta } from '@storybook/react'
+import * as Icons from '@vissimo/icons'
+
+type IconProps = {
+  color?: string
+  size?: number
+  autoSize?: boolean
+  svgProps?: React.SVGProps<SVGSVGElement>
+} & Omit<React.HTMLProps<HTMLSpanElement>, 'color' | 'size'>
+
+export default {
+  title: 'Components/Icons',
+  component: Icons.Clock,
+  tags: ['autodocs'],
+} as Meta<IconProps>
+
+export const Icon = (args: IconProps) => <Icons.Clock {...args} />
+
+Icon.args = {
+  color: 'red',
+  size: 32,
+  autoSize: false,
+  svgProps: { className: 'custom-class' },
+}
