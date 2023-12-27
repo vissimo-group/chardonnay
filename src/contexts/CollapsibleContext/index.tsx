fb3f781d525deab02f119580cc0a702e3a401a7b
Joel Almeida <joel.almeida@vissimo.com.br>
2023-12-27 11:06:57 -0300

    TEC-4140: Refactor CollapsibleContext to use useState and useEffect

--- src/contexts/CollapsibleContext/index.tsx
+++ src/contexts/CollapsibleContext/index.tsx
@@ -1,8 +1,35 @@
-import { createContext } from 'react'
+import { createContext, useEffect, useMemo, useState } from 'react'
+import { CollapsibleContextProps, CollapsibleProviderProps } from './types'
 
-const CollapsibleContext = createContext({
+const CollapsibleContext = createContext<CollapsibleContextProps>({
   isCollapsed: false,
   toggleIsCollapsed: () => {},
 })
 
-export { CollapsibleContext }
+const CollapsibleProvider = ({
+  children,
+  isCollapsed: collapsed,
+}: CollapsibleProviderProps) => {
+  const [isCollapsed, setIsCollapsed] = useState(collapsed)
+
+  const toggleIsCollapsed = () => {
+    setIsCollapsed((state) => !state)
+  }
+
+  const value = useMemo(
+    () => ({ isCollapsed, toggleIsCollapsed }),
+    [isCollapsed],
+  )
+
+  useEffect(() => {
+    setIsCollapsed(collapsed)
+  }, [collapsed])
+
+  return (
+    <CollapsibleContext.Provider value={value}>
+      {children}
+    </CollapsibleContext.Provider>
+  )
+}
+
+export { CollapsibleContext, CollapsibleProvider }
