5dd050867c57f0f0001906e75b625e5e21e8c87e
Joel Almeida <joel.almeida@vissimo.com.br>
2024-01-09 14:36:53 -0300

    TEC-4190: Refactor Select component to include additional props

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,9 +1,22 @@
 import { SelectStyled } from './style'
 import { SelectProps } from './types'
 
-const Select = ({ children, error = false, ...props }: SelectProps) => {
+const Select = ({
+  children,
+  error = false,
+  fontSize = 1,
+  expand = false,
+  responsible = false,
+  ...props
+}: SelectProps) => {
   return (
-    <SelectStyled $error={error} {...props}>
+    <SelectStyled
+      $error={error}
+      $fontSize={fontSize}
+      $expand={expand}
+      $responsible={responsible}
+      {...props}
+    >
       {children}
     </SelectStyled>
   )
