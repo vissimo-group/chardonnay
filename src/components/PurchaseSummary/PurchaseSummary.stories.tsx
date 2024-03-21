a5d7b936e3ba70addb43fc266d63fd6bb021e69e
Gilson Oliveira <juninhokaponnemg@hotmail.com>
2024-03-21 09:20:52 -0300

    TEC-4141: create stories's PurchaseSummary component and types

---
+++ src/components/PurchaseSummary/PurchaseSummary.stories.tsx
@@ -0,0 +1,87 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { PurchaseSummary } from './index'
+import { PurchaseSummaryProps } from './type'
+
+export default {
+  title: 'Components/PurchaseSummary',
+  component: PurchaseSummary,
+  tags: ['autodocs'],
+  argTypes: {
+    description: {
+      control: 'text',
+    },
+    totalitems: {
+      control: 'text',
+    },
+    productsPrice: {
+      control: 'number',
+    },
+    deliveryCost: {
+      control: 'number',
+    },
+    totalDiscount: {
+      control: 'number',
+    },
+    totalPrice: {
+      control: 'number',
+    },
+    installmentPayment: {
+      control: 'text',
+    },
+    discounts: {
+      control: 'object',
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ padding: '16px', backgroundColor: '#f0f0f0' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta<PurchaseSummaryProps>
+
+type Story = StoryObj<PurchaseSummaryProps>
+
+export const EmptyDiscounts: Story = {
+  args: {
+    productsLabel: 'Valor dos produtos',
+    deliveryValue: 'Valor da entrega',
+    totalitems: 2,
+    productsPrice: 178.6,
+    deliveryCost: 15.0,
+    totalDiscount: 29.35,
+    totalPrice: 90,
+    installmentPayment: '2x de 59,85 sem juros',
+    discounts: [],
+  },
+  render: (args) => <PurchaseSummary {...args} />,
+}
+
+export const Default: Story = {
+  name: 'Default',
+  args: {
+    title: '',
+    productsLabel: 'Valor dos produtos',
+    deliveryValue: 'Valor da entrega',
+    totalitems: 1,
+    productsPrice: 178.6,
+    deliveryCost: 15.0,
+    totalDiscount: 29.35,
+    totalPrice: 90,
+    installmentPayment: '2x de 59,85 sem juros',
+    discounts: [
+      {
+        id: 1,
+        title: 'Cupom de desconto',
+        value: '31,00',
+      },
+      {
+        id: 2,
+        title: 'Cashback',
+        value: '11,50',
+      },
+    ],
+  },
+  render: (args) => <PurchaseSummary {...args} />,
+}
---
+++ src/components/PurchaseSummary/type.ts
@@ -0,0 +1,20 @@
+type PurchaseSummaryProps = {
+  title: string
+  productsLabel: string
+  deliveryValue: string
+  totalitems: number
+  productsPrice: number
+  deliveryCost: number
+  totalDiscount: number
+  totalPrice: number
+  installmentPayment: string
+  discounts: Array<DiscountItem>
+}
+
+type DiscountItem = {
+  id: number;
+  title: string;
+  value: string;
+}
+
+export type { PurchaseSummaryProps, DiscountItem }
