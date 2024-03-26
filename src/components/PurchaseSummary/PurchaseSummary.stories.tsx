a55ae1268c3b4b12472d24935150b92824c51337
juninhokaponne <juninhokaponnemg@hotmail.com>
2024-03-26 16:11:25 -0300

    TEC-4141: Added descriptions into the args and it was created a mapping for discounts property

--- src/components/PurchaseSummary/PurchaseSummary.stories.tsx
+++ src/components/PurchaseSummary/PurchaseSummary.stories.tsx
@@ -7,29 +7,66 @@ export default {
   component: PurchaseSummary,
   tags: ['autodocs'],
   argTypes: {
-    description: {
+    title: {
       control: 'text',
+      description: 'Title of the component',
     },
     totalitems: {
       control: 'text',
+      description: 'Total number of items',
     },
     productsPrice: {
       control: 'number',
+      description: 'Price of the products',
     },
     deliveryCost: {
       control: 'number',
+      description: 'Price of the delivery Cost',
     },
     totalDiscount: {
       control: 'number',
+      description: 'Total value of all discounts applied',
     },
     totalPrice: {
-      control: 'number',
+      control: 'Total value of the purchase summary',
+      description: 'Total value of the purchase summary',
     },
     installmentPayment: {
       control: 'text',
+      description: 'Number of installments and monthly amount',
     },
     discounts: {
       control: 'object',
+      description: 'Array of discounts applied to the purchase',
+      label: {
+        options: ['Cupom de desconto', 'Cashback'],
+        mapping: {
+          'Cupom de desconto': {
+            title: 'Cupom de desconto',
+            price: '31,00',
+          },
+          Cashback: {
+            title: 'Cashback',
+            price: '11,50',
+          },
+        },
+      },
+    },
+    deliveryValue: {
+      control: 'text',
+      description: 'Label for the delivery value',
+    },
+    productsLabel: {
+      control: 'text',
+      description: 'Label for the products value',
+    },
+    totalItems: {
+      control: 'number',
+      description: 'Total number of items',
+    },
+    labelItensText: {
+      control: 'text',
+      description: 'Label for the items',
     },
   },
   decorators: [
@@ -43,29 +80,13 @@ export default {
 
 type Story = StoryObj<PurchaseSummaryProps>
 
-export const EmptyDiscounts: Story = {
-  args: {
-    productsLabel: 'Valor dos produtos',
-    deliveryValue: 'Valor da entrega',
-    totalitems: 2,
-    labelItensText: 'itens',
-    productsPrice: 178.6,
-    deliveryCost: 15.0,
-    totalDiscount: 29.35,
-    totalPrice: 90,
-    installmentPayment: '2x de 59,85 sem juros',
-    discounts: [],
-  },
-  render: (args) => <PurchaseSummary {...args} />,
-}
-
-export const Default: Story = {
-  name: 'Default',
+export const WithDiscounts: Story = {
+  name: 'WithDiscounts',
   args: {
     title: '',
     productsLabel: 'Valor dos produtos',
     deliveryValue: 'Valor da entrega',
-    totalitems: 1,
+    totalItems: 1,
     labelItensText: 'item',
     productsPrice: 178.6,
     deliveryCost: 15.0,
@@ -84,6 +105,22 @@ export const Default: Story = {
         price: '11,50',
       },
     ],
+  },
+  render: (args) => <PurchaseSummary {...args} />,
+}
+
+export const WithoutDiscounts: Story = {
+  args: {
+    productsLabel: 'Valor dos produtos',
+    deliveryValue: 'Valor da entrega',
+    totalItems: 2,
+    labelItensText: 'itens',
+    productsPrice: 178.6,
+    deliveryCost: 15.0,
+    totalDiscount: 29.35,
+    totalPrice: 90,
+    installmentPayment: '2x de 59,85 sem juros',
+    discounts: [],
   },
   render: (args) => <PurchaseSummary {...args} />,
 }
