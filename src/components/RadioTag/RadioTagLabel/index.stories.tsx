955bee20baad0af60115c043b5c414b085da8d59
Joel Almeida <joel.almeida@vissimo.com.br>
2024-04-01 14:43:29 -0300

    TEC-4386: Add RadioTagLabel component and its stories.

---
+++ src/components/RadioTag/RadioTagLabel/index.stories.tsx
@@ -0,0 +1,96 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { RadioTagLabel } from '.'
+import { RadioTagInput } from '../RadioTagInput'
+
+const meta: Meta<typeof RadioTagLabel> = {
+  component: RadioTagLabel,
+  tags: ['autodocs'],
+  argTypes: {
+    width: { control: 'text' },
+  },
+  parameters: {
+    docs: {
+      controls: { exclude: ['ref', 'theme', 'as', 'forwardedAs'] },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ display: 'flex', gap: '1rem' }}>
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export default meta
+type Story = StoryObj<typeof RadioTagLabel>
+
+export const Checked: Story = {
+  args: { width: '9rem' },
+  render: (args) => (
+    <>
+      <RadioTagInput checked id="one" name="my-radio-tag-v" />
+      <RadioTagLabel {...args} htmlFor="one">
+        Option One
+      </RadioTagLabel>
+
+      <span style={{ textWrap: 'balance', textAlign: 'center' }}>
+        The <strong>RadioTagLabel</strong> component must always be preceded by
+        a <strong>RadioTagInput</strong> for styles to be applied correctly.
+      </span>
+    </>
+  ),
+}
+
+export const Default: Story = {
+  render: () => (
+    <>
+      <RadioTagInput id="one" name="my-radio-tag-iii" />
+      <RadioTagLabel htmlFor="one">Label One</RadioTagLabel>
+
+      <RadioTagInput id="two" name="my-radio-tag-iii" />
+      <RadioTagLabel htmlFor="two">Label Two</RadioTagLabel>
+
+      <RadioTagInput id="three" name="my-radio-tag-iii" />
+      <RadioTagLabel htmlFor="three">Label Three</RadioTagLabel>
+
+      <RadioTagInput id="four" name="my-radio-tag-iii" />
+      <RadioTagLabel htmlFor="four">Label Four</RadioTagLabel>
+
+      <RadioTagInput id="five" name="my-radio-tag-iii" />
+      <RadioTagLabel htmlFor="five">Label Three</RadioTagLabel>
+    </>
+  ),
+}
+
+export const WithFixedWidth: Story = {
+  args: { width: '10rem' },
+  render: (args) => (
+    <>
+      <RadioTagInput id="one" name="my-radio-tag-iv" />
+      <RadioTagLabel {...args} htmlFor="one">
+        Option One
+      </RadioTagLabel>
+
+      <RadioTagInput id="two" name="my-radio-tag-iv" />
+      <RadioTagLabel {...args} htmlFor="two">
+        Option Two
+      </RadioTagLabel>
+
+      <RadioTagInput id="three" name="my-radio-tag-iv" />
+      <RadioTagLabel {...args} htmlFor="three">
+        Option Three
+      </RadioTagLabel>
+
+      <RadioTagInput id="four" name="my-radio-tag-iv" />
+      <RadioTagLabel {...args} htmlFor="four">
+        Option Four
+      </RadioTagLabel>
+
+      <RadioTagInput id="five" name="my-radio-tag-iv" />
+      <RadioTagLabel {...args} htmlFor="five">
+        Option Five
+      </RadioTagLabel>
+    </>
+  ),
+}
---
+++ src/components/RadioTag/RadioTagLabel/style.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+const RadioTagLabelStyled = styled.label<{ width?: string }>`
+  width: ${(props) => (props.width ? props.width : 'fit-content')};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: fit-content;
+  gap: 0.5em;
+  border-radius: 2rem;
+  padding: 0.5em 1em;
+  background-color: ${(props) => props.theme.colors.neutral.neutral200};
+  color: ${(props) => props.theme.colors.neutral.neutral400};
+  border: 2px solid ${(props) => props.theme.colors.neutral.neutral200};
+  scroll-snap-align: center;
+
+  input[type='radio']:checked + & {
+    background-color: ${(props) => props.theme.colors.background.background200};
+    border: 2px solid ${(props) => props.theme.colors.action.action100};
+    color: ${(props) => props.theme.colors.action.action100};
+  }
+
+  // TODO: ADJUST DISABLED STATE
+  input[type='radio']:disabled + & {
+    cursor: not-allowed;
+    border: 1px solid red;
+  }
+
+  &:not(:disabled) {
+    cursor: pointer;
+  }
+`
+export { RadioTagLabelStyled }
