d40374b45d05c231010c2e4dc470ff525a166ba9
juninhokaponne <juninhokaponnemg@hotmail.com>
2024-04-09 09:59:59 -0300

    TEC-4290: Create the input folder and the component

---
+++ src/components/PaymentMethods/PaymentMethodsInput/index.tsx
@@ -0,0 +1,11 @@
+import { PaymentMethodsInputFormGroup, PaymentMethodInputStyled } from './style'
+
+const PaymentMethodsInput = () => {
+  return (
+    <PaymentMethodsInputFormGroup>
+      <PaymentMethodInputStyled />
+    </PaymentMethodsInputFormGroup>
+  )
+}
+
+export { PaymentMethodsInput }
