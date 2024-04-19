2e3b2d09cdd1c55998b75d2da4c8d4762012c890
juninhokaponne <juninhokaponnemg@hotmail.com>
2024-04-19 09:55:25 -0300

    TEC-4292: it was Created the type for the component

---
+++ src/components/CreditCard/type.ts
@@ -0,0 +1,12 @@
+type CreditCardProps = {
+  name?: string
+  date?: string
+  isFlipped?: boolean
+  CardBanner?: React.ReactNode
+  logoCard?: React.ReactNode
+  backgroundCardColor?: string
+  children: React.ReactNode
+  newCreditCard?: boolean
+}
+
+export type { CreditCardProps }
