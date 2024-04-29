ec929f8bcc65ee15a5d4da4566e043cf17f6f954
juninhokaponne <juninhokaponnemg@hotmail.com>
2024-04-29 09:46:52 -0300

    TEC-4290: new property added

--- src/components/PaymentMethods/PaymentMethodsContainer/type.ts
+++ src/components/PaymentMethods/PaymentMethodsContainer/type.ts
@@ -3,13 +3,12 @@ import React from 'react'
 type PaymentMethodsProps = {
   children: React.ReactNode
   title: string
-  paymentData: {
-    id: number
-    icon: React.ReactNode
-    label: string
-    installment?: string
-    isNewCard?: boolean
-  }[]
+  id: number
+  icon: React.ReactNode
+  label: string
+  labelColor: string
+  installment?: string
+  isNewCard?: boolean
 }
 
 export type { PaymentMethodsProps }
