16b12784bbb13933beb04c9e1ca26a8a19b3a35a
juninhokaponne <juninhokaponnemg@hotmail.com>
2024-05-16 14:38:06 -0300

    TEC-4290: fixed comments on pr

--- src/components/PaymentMethods/PaymentMethodsSubItems/style.tsx
+++ src/components/PaymentMethods/PaymentMethodsSubItems/style.tsx
@@ -6,7 +6,6 @@ const PaymentMethodsSubItemsStyled = styled.div`
     gap: 1rem;
 
     @media screen and (max-width: ${mediaQueries.screenXs}) {
-      display: flex;
       margin-top: 8rem;
       position: absolute;
       width: 22.85rem;
@@ -15,7 +14,6 @@ const PaymentMethodsSubItemsStyled = styled.div`
     }
 
     @media screen and (max-width: ${mediaQueries.screenXxs}) {
-      display: flex;
       margin-top: 8.625rem;
       position: absolute;
       width: 16.85rem;
